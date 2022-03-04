050eddfb1919b8b491f20189b48d2b26c394fc2a
TC <teresa.j.campbell@gmail.com>
2022-03-04 09:16:11 -0500

    now functioning

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,6 +35,14 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+  }
+  res.render('login');
+});
+
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
@@ -79,18 +87,7 @@ router.get("/post/:id", (req, res) => {
     });
 });
 
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
 
-  res.render("login");
-});
-
-router.get("/signup", (req, res) => {
-  res.render("signup");
-});
 
 router.get("/posts-comments", (req, res) => {
   Post.findOne({
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -4,11 +4,11 @@ async function loginFormHandler(event) {
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
+    if (username && password) {
       const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
-          email,
+          username,
           password
         }),
         headers: { 'Content-Type': 'application/json' }
