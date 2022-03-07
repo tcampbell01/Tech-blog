5f0da6b00251bd700ea386f78c664f7a01f19300
TC <teresa.j.campbell@gmail.com>
2022-03-06 20:57:56 -0500

    changed to lowercase

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const Post = require('./post');
-const User = require('./User');
-const Comment = require('./Comment');
+const User = require('./user');
+const Comment = require('./comment');
 
 // create associations
 User.hasMany(Post, {
